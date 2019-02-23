b60a3de6c7a8c307b82191d41026e344b06ee717
joepound <14357797+joepound@users.noreply.github.com>
2019-02-22 23:37:18 -0700

    Test for deploy

--- middleware/routes/rootRouter.js
+++ middleware/routes/rootRouter.js
@@ -5,6 +5,11 @@ router.get("/", (req, res) => {
   res.send(`
     <h1>GTD API</h1>
     <p>Welcome to the GTD API!</p>
+    <ul>
+      ${Object.keys(process.env).map(
+        key => "<li>" + key + " " + process.env[key] + "</li>"
+      )}
+    </ul>
   `);
 });
 
