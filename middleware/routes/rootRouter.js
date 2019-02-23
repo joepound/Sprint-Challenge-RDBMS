99c3d9e8e577fc3b6bba7ac72e9ea0e718ec6435
joepound <14357797+joepound@users.noreply.github.com>
2019-02-23 02:43:37 -0700

    Stop displaying environment variables in root route

--- middleware/routes/rootRouter.js
+++ middleware/routes/rootRouter.js
@@ -5,11 +5,6 @@ router.get("/", (req, res) => {
   res.send(`
     <h1>GTD API</h1>
     <p>Welcome to the GTD API!</p>
-    <ul>
-      ${Object.keys(process.env).map(
-        key => "<li>" + key + " " + process.env[key] + "</li>"
-      )}
-    </ul>
   `);
 });
 
